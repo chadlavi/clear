845fe8e060ee16dc2a64d7db1be2a18ab1e98101
Chad Lavimoniere <chad@chadlavimoniere.com>
2020-04-26 03:11:12 -0400

    add ./docs

---
+++ docs/precache-manifest.b7b0402f01dc27c7c419f8f7434e78a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f093b95701dbcbe0421763bf02172d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c68a50effbb506bfb104",
+    "url": "/static/js/2.a7021c83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a7021c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c1b20da81e1c011e6f7",
+    "url": "/static/js/main.e23f9e0f.chunk.js"
+  },
+  {
+    "revision": "62966cde030fd6bd2032",
+    "url": "/static/js/runtime-main.6ab19328.js"
+  }
+]);
