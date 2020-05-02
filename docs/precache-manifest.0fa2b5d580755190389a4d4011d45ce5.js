f0c98f4dd466dabf8c6eb4be1f70c3b6b93a0822
Chad Lavimoniere <chad@chadlavimoniere.com>
2020-05-01 23:43:08 -0400

    update docs

---
+++ docs/precache-manifest.0fa2b5d580755190389a4d4011d45ce5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a021df947840d0d71a3083e9205d5330",
+    "url": "/clear/index.html"
+  },
+  {
+    "revision": "b1bfdd31e3e2664d9735",
+    "url": "/clear/static/js/2.a39dd3d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clear/static/js/2.a39dd3d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc24bd486f83836ba2f",
+    "url": "/clear/static/js/main.56f856d6.chunk.js"
+  },
+  {
+    "revision": "0ec4d3f08f2ba62dad17",
+    "url": "/clear/static/js/runtime-main.80255c42.js"
+  }
+]);
