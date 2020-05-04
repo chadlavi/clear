60ef74be363ebaca380b4d3c4d4b2fb96402f4af
Chad Lavimoniere <chad@chadlavimoniere.com>
2020-05-03 20:30:27 -0400

    separate out input and label, add SPA-safe skiplink

---
+++ src/components/Label/Label.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  /**
+   * if true, the `<Label>` is shown with disabled styling
+   */
+  disabled?: boolean
+  /**
+   * if true, the `<Label>` is shown with error styling
+   */
+  error?: boolean
+  /**
+   * if true, `<Label>` is visually marked as required. Should match the value of the `required` prop on the
+   * corresponding form field
+   */
+  required?: boolean
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const LabelBase = ({disabled, error, required, ...props}: LabelProps): JSX.Element => (
+  <label
+    {...props}
+  >
+    {required ? <>{props.children}{' (Required)'}</> : props.children}
+  </label>
+)
+
+/**
+ * A simple styled `<label>`
+ *
+ * https://chadlavi.github.io/clear/#/text#label
+ */
+export const Label = styled(LabelBase)`
+  color: ${(p): string => p.error ? 'var(--clear-error)' : 'inherit'};
+  ${(p): string  => p.disabled ? `
+    opacity: 0.5;
+  ` : ''};
+  width: 100%;
+  font-size: var(--clear-font-size-label);
+`
---
+++ src/components/SkipLink/SkipLink.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import {Button} from '../Button'
+import styled from 'styled-components'
+
+const jumpTo = (id: string) => (): void => {
+  const el = document.getElementById(id)
+  if (el) {
+    el.scrollIntoView()
+    el.focus()
+  }
+}
+
+const SkipLinkButton = styled(Button)`
+  position: absolute;
+  top: var(--clear-unit);
+  left: var(--clear-unit);
+  opacity: 0;
+  pointer-events: none;
+  user-select: none;
+  :focus {
+    opacity: 1;
+    pointer-events: unset;
+    user-select: unset;
+  }
+  
+`
+
+export const SkipLink = ({id}: {id: string}): JSX.Element => {
+
+  return (
+    <SkipLinkButton
+      onClick={jumpTo(id)}
+      primary
+    >
+      Skip to main content
+    </SkipLinkButton>
+  )
+}
