e4676a1544ea5b17041fe86656dbe86aff1ed84e
Chad Lavimoniere <chad@chadlavimoniere.com>
2020-05-11 23:42:54 -0400

    add green (related to #3)

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -4,6 +4,7 @@ export type Colors = 'background' |
 'blue' |
 'border' |
 'error' |
+'green' |
 'link' |
 'textColor' |
 'violet' |
@@ -15,17 +16,19 @@ export const colors: {[key in Themes]: {[key in Colors]: string}} = {
     blue: '#005cb8',
     border: '#ddd',
     error: '#cf0303',
+    green: '#067940',
     link: '#55e',
     textColor: '#111',
     violet: '#bc1abc',
     zebra: '#f8f8f8',
   },
   dark: {
-    background: '#111',
+    background: '#0a0a0a',
     blue: 'dodgerblue',
     border: '#555',
-    error: '#ff5656',
-    link: '#8787f9',
+    error: '#f99',
+    green: '#40bf80',
+    link: '#9e9efa',
     textColor: 'white',
     violet: 'violet',
     zebra: '#242424',
